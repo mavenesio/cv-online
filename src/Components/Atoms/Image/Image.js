c2f0c645e387dfd885b4f8b0ec10eb32f2a6a82e
mariano <mavenesio@gmail.com>
2020-02-08 12:32:24 -0300

    add image atom

---
+++ src/Components/Atoms/Image/Image.js
@@ -0,0 +1,16 @@
+
+import styled from 'styled-components';
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+  display: block;
+  padding: 0;
+  margin: 0;
+  
+  &.logo {
+    height: 20vmin;
+    pointer-events: none;
+    margin-bottom: 50px;
+  }
+`;
