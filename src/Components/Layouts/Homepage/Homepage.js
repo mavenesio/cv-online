b0ad1ec7d2e07613800ca34dc23787297b69795c
mariano <mavenesio@gmail.com>
2020-02-10 18:56:31 -0300

    add skill list into the homepage

--- src/Components/Layouts/Homepage/Homepage.js
+++ src/Components/Layouts/Homepage/Homepage.js
@@ -9,10 +9,47 @@ import SkillList from '../../Molecules/SkillList/SkillList';
 import kermit from '../../../assets/Kermit.png';
 import Card from '../../Atoms/Card/Card';
 
+import angular from '../../../assets/Logos/angular.svg';
+import css from '../../../assets/Logos/css.svg';
+import html from '../../../assets/Logos/html.svg';
+import javascript from '../../../assets/Logos/javascript.svg';
+import react from '../../../assets/Logos/react.svg';
+
+import mongo from '../../../assets/Logos/mongo.svg';
+import netcore from '../../../assets/Logos/netcore.svg';
+import sql from '../../../assets/Logos/sql.svg';
+import node from '../../../assets/Logos/node.svg';
+
+import git from '../../../assets/Logos/git.png';
+import scrum from '../../../assets/Logos/scrum.svg';
+import macos from '../../../assets/Logos/macos.svg';
+
+import location from '../../../assets/Logos/location.svg';
+import email from '../../../assets/Logos/email.svg';
+import phone from '../../../assets/Logos/phone.png';
+import user from '../../../assets/Logos/user.png';
+
 const description = 'Estudiante de Licenciatura en Análisis de Sistemas (UBA) con tres años de experiencia como desarrollador web FullStack. Soy capaz de aprender de forma constante y responsable, una persona proactiva con capacidad de adaptarse a los cambios. Siempre busco obtener el mejor resultado posible para mi equipo, aportando nuevas ideas y soluciones. Puedo captar rápidamente nuevas tecnologías y diferentes procesos.'
-const FrontEndSkills = ['JavaScript','AngularJs','Angular7','HTML5','CSS3'];
-const BackEndSkills = ['NodeJs','.NET','SQL/MySql/PostgreSQL','MongoDB'];
-const OtherSkills = ['SVN/TFS/GIT','metodologiasagiles/Scrum/Canvas','MacOS/Ubuntu/','Photoshop'];
+const FrontEndSkills = [
+    {name:'JavaScript', logo:javascript},
+    {name:'AngularJs', logo:angular},
+    {name:'Angular 8', logo:angular},
+    {name:'React', logo:react},
+    {name:'HTML5', logo:html},
+    {name:'CSS3', logo:css}
+];
+
+const BackEndSkills = [
+    {name:'NodeJs', logo:node},
+    {name:'.NET', logo:netcore},
+    {name:'SQL/MySql/PostgreSQL', logo:sql},
+    {name:'MongoDB', logo:mongo}];
+
+const OtherSkills = [
+    {name:'SVN/TFS/GIT', logo:git},
+    {name:'Agile/Scrum/Canvas', logo:scrum},
+    {name:'MacOS/Ubuntu', logo:macos},
+    {name:'Photoshop', logo:angular}];
 const experiences = [
 	{
 		companyName:'Codea IT',
@@ -39,7 +76,11 @@ const experiences = [
 		jobUbication:'Ciudad Autónoma de BuenosAires, Argentina',
 	}
 ]
-const contactData = ['28 años, Argentino ','(011)1556589904','mavenesio@gmail.com','Palermo,CABA,Argentina'];
+const contactData = [
+    {name:'28 años, Argentino ', logo:user},
+    {name:'(011)1556589904', logo:phone},
+    {name:'mavenesio@gmail.com', logo:email},
+    {name:'Palermo, CABA, Argentina', logo:location}];
 
 const HomepageContainer = styled.div`
 `;
@@ -76,7 +117,7 @@ const SecondaryColumn = styled.div`
     flex-direction: column;
     justify-content:flex-start;
     flex-basis: 100%;
-    flex: 3;
+    flex: 4;
     padding: 1rem;
 `;
 const Image = styled.img`
